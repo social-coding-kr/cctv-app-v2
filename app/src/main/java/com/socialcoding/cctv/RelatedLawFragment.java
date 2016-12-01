d64d3e7baa6d096c2783b1030ae965b027a49ae2
RYUHA <ryuha@RYUHAui-MacBook-Pro.local>
2016-12-01 22:57:35 +0900

    Add webview.

--- app/src/main/java/com/socialcoding/cctv/RelatedLawFragment.java
+++ app/src/main/java/com/socialcoding/cctv/RelatedLawFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import com.socialcoding.models.Law;
@@ -18,12 +20,12 @@ import java.util.List;
 /**
  * Created by yoon on 2016. 10. 22..
  */
-public class RelatedLawFragment extends Fragment implements View.OnClickListener {
-    private List<Law> LawList = new ArrayList<>();
+public class RelatedLawFragment extends Fragment {
 
-    private View singleLawLayout;
-    private TextView singleLawTitleTextView;
-    private TextView singleLawContentTextView;
+    private MainActivity mainActivity;
+
+    private WebView mWebView; // 웹뷰
+    private WebSettings mWebSettings; // 웹뷰 세팅
 
     @Nullable
     @Override
@@ -34,59 +36,20 @@ public class RelatedLawFragment extends Fragment implements View.OnClickListener
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView titleTextView = (TextView) getActivity().findViewById(R.id.title_text_view);
-        titleTextView.setText(getActivity().getResources().getString(R.string.related_law_string));
-        try {
-            initAfterViewCreated();
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (!(getActivity() instanceof MainActivity)) {
+            throw new RuntimeException("Invalid activity.");
         }
-    }
+        mainActivity = (MainActivity) getActivity();
 
-    private void initAfterViewCreated() throws Exception {
-        fetchLawList();
-        for(Law law : LawList) {
-            findLayoutsByLaw(law);
-            initTexts(law);
-            initButtonLayouts();
-        }
-    }
+        TextView titleTextView = (TextView) mainActivity.findViewById(R.id.title_text_view);
+        titleTextView.setText(getResources().getString(R.string.related_law_string));
 
-    private void fetchLawList() {
-        Collections.addAll(LawList, new Law(R.id.law_layout_1, R.raw.law_title_1, R.raw.law_content_1),
-                new Law(R.id.law_layout_2, R.raw.law_title_2, R.raw.law_content_2),
-                new Law(R.id.law_layout_3, R.raw.law_title_3, R.raw.law_content_3),
-                new Law(R.id.law_layout_4, R.raw.law_title_4, R.raw.law_content_4),
-                new Law(R.id.law_layout_5, R.raw.law_title_5, R.raw.law_content_5));
-    }
+        mWebView = (WebView) mainActivity.findViewById(R.id.webview); // 레이어와 연결
+        //mWebView.setWebViewClient(new WebViewClient()); // 클릭 시 새창 안뜨게
+        mWebSettings = mWebView.getSettings(); // 세부 세팅 등록
+        mWebSettings.setJavaScriptEnabled(true); // 자바스크립트 사용 허용
 
-    private void findLayoutsByLaw(Law law) {
-        // Ryuha commit
-        singleLawLayout = getView().findViewById(law.getSingleLawLayoutId()); // after onCreateView. It is safe.
-        singleLawTitleTextView = (TextView) singleLawLayout.findViewById(R.id.law_title_text);
-        singleLawContentTextView = (TextView) singleLawLayout.findViewById(R.id.law_content_text);
-    }
-
-    private void initTexts(Law law) throws Exception {
-        singleLawTitleTextView.setText(Utilities.rawTxtToString
-                .convert(getResources().openRawResource(law.getSingleLawTitleTxtId())));
-        singleLawContentTextView.setText(Utilities.rawTxtToString
-                .convert(getResources().openRawResource(law.getSingleLawContentTxtId())));
-    }
-
-    private void initButtonLayouts() {
-        singleLawLayout.setClickable(false);
-        singleLawTitleTextView.setOnClickListener(this);
-        singleLawContentTextView.setLinksClickable(false);
-    }
-
-    @Override
-    public void onClick(View v) {
-        v = (View) v.getParent();
-        if(v.findViewById(R.id.law_content_text).getVisibility() == View.GONE) {
-            v.findViewById(R.id.law_content_text).setVisibility(View.VISIBLE);
-        } else {
-            v.findViewById(R.id.law_content_text).setVisibility(View.GONE);
-        }
+        //원하는 url 입력 -> 나중에 사용자가 입력하는걸로 받으면 되겠다
+        mWebView.loadUrl("file:///android_asset/tutorial_test.html");
     }
 }
