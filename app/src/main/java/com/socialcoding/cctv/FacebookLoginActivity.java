25ea98862f94105f32a868c37a6771ded3ea8ed1
yoon <foryou8904@naver.com>
2017-02-28 00:25:37 +0900

    Add facebook login.

---
+++ app/src/main/java/com/socialcoding/cctv/FacebookLoginActivity.java
@@ -0,0 +1,77 @@
+package com.socialcoding.cctv;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+/**
+ * Created by yoon on 2017. 2. 18..
+ */
+
+public class FacebookLoginActivity extends AppCompatActivity
+        implements FacebookCallback<LoginResult>
+{
+    @BindView(R.id.button_login_facebook) LoginButton loginButton;
+    @BindView(R.id.button_login_skip) Button skipButton;
+
+    CallbackManager callbackManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppEventsLogger.activateApp(getApplication());
+        setContentView(R.layout.activity_login_facebook);
+
+        ButterKnife.bind(this);
+
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, this);
+    }
+
+    // FacebookCallback<LoginResult>
+    @Override
+    public void onSuccess(LoginResult loginResult) {
+        startMainActivity();
+    }
+
+    // FacebookCallback<LoginResult>
+    @Override
+    public void onCancel() {
+    }
+
+    // FacebookCallback<LoginResult>
+    @Override
+    public void onError(FacebookException error) {
+    }
+
+    @OnClick(R.id.button_login_skip)
+    void onSkipButtonClicked() {
+        startMainActivity();
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
