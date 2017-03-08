317021f83986ec4fb5326788e0add97599f1bdeb
조지영 <jojiiiiyoung@gmail.com>
2017-03-08 22:50:59 +0900

    fabric 추가

--- app/src/main/java/com/socialcoding/cctv/SplashActivity.java
+++ app/src/main/java/com/socialcoding/cctv/SplashActivity.java
@@ -3,6 +3,8 @@ package com.socialcoding.cctv;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
@@ -13,6 +15,7 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
 
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/NanumBarunGothic_Regular.ttf")
