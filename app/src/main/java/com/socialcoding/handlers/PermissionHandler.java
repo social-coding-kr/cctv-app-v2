197024c442bb457199b2a7ab84296ad4ee46467c
RYUHA <ryuha@RYUHAui-MacBook-Pro.local>
2017-03-23 00:11:12 +0900

    위치정보동의 디자인

--- app/src/main/java/com/socialcoding/handlers/PermissionHandler.java
+++ app/src/main/java/com/socialcoding/handlers/PermissionHandler.java
@@ -3,12 +3,19 @@ package com.socialcoding.handlers;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.socialcoding.cctv.R;
 import com.socialcoding.vars.EyeOfSeoulPermissions;
 
@@ -45,6 +52,7 @@ public class PermissionHandler {
         } else {
             if(needExplanation()) {
                 explain();
+
             } else {
                 askPermission();
             }
@@ -64,28 +72,60 @@ public class PermissionHandler {
     public void explain() {
         // Toast or dialog.
 
-        new AlertDialog.Builder(activity)
-                .setMessage(R.string.permission_reason_explaination)
-                .setCancelable(false)
-                .setNegativeButton(R.string.permission_btn_close, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                }).setPositiveButton(R.string.permission_btn_setting, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        try {
-                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                                    .setData(Uri.parse("package:" + activity.getPackageName()));
-                            activity.startActivity(intent);
-                        } catch (ActivityNotFoundException e) {
-                            e.printStackTrace();
-                            Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_APPLICATIONS_SETTINGS);
-                            activity.startActivity(intent);
-                        }
-                    }
-                }).show();
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.redialog_agreement_layout, null);
+
+        final AlertDialog alertDialogPermission = new AlertDialog.Builder(activity).create();
+
+        Button settingBtn = (Button) alertLayout.findViewById(R.id.button_setting);
+        Button closeBtn = (Button) alertLayout.findViewById(R.id.button_close);
+
+        settingBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                            .setData(Uri.parse("package:" + activity.getPackageName()));
+                    activity.startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    e.printStackTrace();
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_APPLICATIONS_SETTINGS);
+                    activity.startActivity(intent);
+                }
+            }
+        });
+
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                alertDialogPermission.dismiss();
+            }
+        });
+
+        alertDialogPermission.setView(alertLayout);
+        alertDialogPermission.show();
+
+//        new AlertDialog.Builder(activity)
+//                .setMessage(R.string.permission_reason_explaination)
+//                .setCancelable(false)
+
+//                .setNegativeButton(R.string.permission_btn_close, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        dialogInterface.dismiss();
+//                    }
+//                }).setPositiveButton(R.string.permission_btn_setting, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        try {
+//                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+//                                    .setData(Uri.parse("package:" + activity.getPackageName()));
+//                            activity.startActivity(intent);
+//                        } catch (ActivityNotFoundException e) {
+//                            e.printStackTrace();
+//                            Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_APPLICATIONS_SETTINGS);
+//                            activity.startActivity(intent);
+//                        }
+//                    }
+//                }).show();
 
     }
 
